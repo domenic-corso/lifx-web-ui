28d320fb86f1820264799b1ee088ee31de35465c
Domenic Corso <domenic.corso@live.com>
2020-06-24 18:29:07 +1000

    Make use of .panel for LightManager and add refresh button

--- frontend/components/LightManager.svelte
+++ frontend/components/LightManager.svelte
@@ -3,37 +3,44 @@
     import LightService from '../services/LightService.js';
 
     let lights = [];
+    let refreshingLights = true;
 
     /**
      * Refreshes the list of lights
      */
     function refreshLights() {
+        refreshingLights = true;
+
         // Fetch all lights
         LightService.get().then(data => {
             lights = data;
+        }).finally(() => {
+            refreshingLights = false;
         });
-    }
-
-    /**
-     * Handles cases where a light has been updated.
-     */
-    function handleLightUpdated() {
-        refreshLights();
     }
 
     refreshLights();
 </script>
 
 <style>
-    .lights {
+    .lights__container {
         display: flex;
+        padding-top: 32px;
     }
 </style>
 
-<h2>Lights</h2>
-
-<div class="lights">
-    {#each lights as light}
-        <Light {...light} on:lightUpdated={handleLightUpdated} />
-    {/each}
+<div class="panel">
+    <header class="panel__header">
+        <h2 class="panel__title">Lights</h2>
+        <div class="panel__options">
+            <button class="optbutton" class:optbutton--spinning="{refreshingLights}" type="button" on:click={refreshLights}>
+                <i class="material-icons">refresh</i>
+            </button>
+        </div>
+    </header>
+    <div class="panel__content lights__container">
+        {#each lights as light}
+            <Light {...light} on:lightUpdated={refreshLights} />
+        {/each}
+    </div>
 </div>
